33685093f62f7a02242f543ddf46bdd806675fff
Bhaskar Nath <137477778+BhaskarNath27@users.noreply.github.com>
2026-06-27 21:12:57 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.22%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt=0;
+        int n=word.length();
+
+        for(int i=0;i<n;i++){
+            if(isupper(word[i])){
+                cnt++;
+            }
+
+
+        }
+        if(cnt==n) return true;
+        else if(cnt==0) return true;
+        else if(cnt==1 && isupper(word[0])) return true;
+        else return false;
+    }
+};
