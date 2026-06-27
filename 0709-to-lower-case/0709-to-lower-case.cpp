e16690eeda2befbd34b7942b84ee6525f36ca5a2
Bhaskar Nath <137477778+BhaskarNath27@users.noreply.github.com>
2026-06-27 20:22:26 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.06%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.length();i++){
-            s[i]=tolower(s[i]);
+            if(!islower(s[i])){
+                s[i]=tolower(s[i]);
+            }
+            
         }
         return s;
     }
